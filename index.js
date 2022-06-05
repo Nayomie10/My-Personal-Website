530d08f68d0fce3b24a778fa2a850a10ad35ddb1
Naomi Nyongesa <naomi.nyongesa@student.moringaschool.com>
2022-06-05 23:50:36 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+});
